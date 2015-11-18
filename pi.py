11400a84b2a1f9597f217d328c5e1a3e5723dd8d
nacastellanos <nicolas.castellano.6@gmail.com>
2015-11-18 15:45:22 -0500

    Update pi.py

--- pi.py
+++ pi.py
@@ -2,6 +2,8 @@ import math
 def pifun(d):
   # This function will find pi to the NTH digit, so lets get started
   print "Let´s round the pi number!!"
-  pinum = math.pi()
+  pinum = float(math.pi())
   pinum = round(pinum,d)
   print "The pi number to the %d digits is %d" % (d, pinum)
+
+pifun(3)
